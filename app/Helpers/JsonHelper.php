22de0bf7c114dda0da003379e22e00fadc028ecd
Honza_Slabihoud <jan.slabihoud@4camping.cz>
2023-04-09 20:16:37 +0200

    json helper fix

--- app/Helpers/JsonHelper.php
+++ app/Helpers/JsonHelper.php
@@ -3,16 +3,16 @@ declare(strict_types=1);
 
 namespace App\Helpers;
 
-use Nette\Utils\Json;
+use stdClass;
 
 class JsonHelper
 {
     /**
-     * @param Json $json
+     * @param stdClass $json
      * @param string $key
      * @return bool
      */
-    public static function containsKey(Json $json, string $key): bool
+    public static function containsKey(stdClass $json, string $key): bool
     {
         if (isset($json->$key)) {
             return true;
