144a5a6e202c4fb74d1a903bcf5017c9d5cf5ab5
Honza_Slabihoud <jan.slabihoud@4camping.cz>
2023-07-26 11:11:26 +0200

    translations

---
+++ lang/en/general.php
@@ -0,0 +1,20 @@
+<?php
+return [
+    'introduction' => 'Development of add-ons for Shoptet e-shops',
+    'subtitle' => 'Below on this page, you will find a list of add-ons I have created for the Shoptet add-on marketplace.',
+
+    'settings' => 'Settings',
+    'save' => 'Save',
+    'saved' => 'Saved',
+    'homepage_button' => 'I want this',
+    'homepage_terms' => 'Terms of Use',
+    'error' => 'An error occurred, please try again later',
+    'terms' => 'Terms of Use',
+    'terms_content_header_1' => 'Definitions',
+    'terms_content_header_2' => 'Introductory Provisions',
+    'terms_content_header_3' => 'License Agreement',
+    'terms_content_header_4' => 'Operation Parameters',
+    'terms_content_header_5' => 'Support and Maintenance',
+    'terms_content_header_6' => 'Liability',
+    'privacy' => 'Privacy Policy',
+];
