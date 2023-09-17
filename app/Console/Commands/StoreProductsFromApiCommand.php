048ba72bc13a4917599a9142fb9eaafeaad4a125
Honza_Slabihoud <jan.slabihoud@4camping.cz>
2023-09-17 18:22:34 +0200

    last_synced skip cmd

--- app/Console/Commands/StoreProductsFromApiCommand.php
+++ app/Console/Commands/StoreProductsFromApiCommand.php
@@ -59,6 +59,9 @@ class StoreProductsFromApiCommand extends AbstractCommand
 
             foreach ($clientServices as $clientService) {
                 $currentClientId = $clientService->getAttribute('client_id');
+                if ($clientService->getAttribute('date_last_synced') <= now()->subHours(12)) {
+                    continue;
+                }
                 $products = Product::where('client_id', $currentClientId)->where('active', true)->get(['id', 'guid', 'active']);
                 for ($page = 1; $page < ResponseHelper::MAXIMUM_ITERATIONS; $page++) { 
                     try {
