dd305590cd0b1969f6b9d175ed19c52ed80815e9
Honza_Slabihoud <jan.slabihoud@4camping.cz>
2023-11-17 13:47:29 +0100

    total refactor, bug fixes

--- resources/js/carousel-1.7.js
+++ resources/js/carousel-1.7.js
@@ -1,22 +1,21 @@
 const pw_carousel_settings = document.getElementById('dynamic-preview-images');
-let pw_i_r = '0';
-let pw_r_t_d ='0';
-let pw_s_t = 1500;
-let pw_i_l = 500;
-let pw_a_t = 'all';
-let pw_m_i = 'circles';
+let pw_infinite_repeat = '0';
+let pw_return_to_default ='0';
+let pw_show_time = 1500;
+let pw_initial_loop = 500;
+let pw_apply_to = 'all';
+let pw_mobile_icons = 'circles';
 if (pw_carousel_settings) {
-  pw_i_r = pw_carousel_settings.getAttribute('data-dynamic-preview-images.infinite_repeat') || '0';
-  pw_r_t_d = pw_carousel_settings.getAttribute('data-dynamic-preview-images.return_to_default') || '0';
-  pw_s_t = pw_carousel_settings.getAttribute('data-dynamic-preview-images.show_time') || 1500;
-  pw_i_l = pw_carousel_settings.getAttribute('data-dynamic-preview-images.initial_loop') || 500;
-  pw_a_t = pw_carousel_settings.getAttribute('data-dynamic-preview-images.apply_to') || 'all';
-  pw_m_i = pw_carousel_settings.getAttribute('data-dynamic-preview-images.mobile_icons') || 'circles';
+  pw_infinite_repeat = pw_carousel_settings.getAttribute('data-dynamic-preview-images.infinite_repeat');
+  pw_return_to_default = pw_carousel_settings.getAttribute('data-dynamic-preview-images.return_to_default');
+  pw_show_time = pw_carousel_settings.getAttribute('data-dynamic-preview-images.show_time');
+  pw_initial_loop = pw_carousel_settings.getAttribute('data-dynamic-preview-images.initial_loop');
+  pw_apply_to = pw_carousel_settings.getAttribute('data-dynamic-preview-images.apply_to');
+  pw_mobile_icons = pw_carousel_settings.getAttribute('data-dynamic-preview-images.mobile_icons');
 }
 let pw_image_prefix;
 let pw_global_products = [];
 let pw_running_interval;
-let pw_is_running = false;
 
 const pw_project_id = getShoptetDataLayer('projectId');
 const pw_products = [];
@@ -24,6 +23,7 @@ let pw_products_response;
 const pw_elements = document.querySelectorAll('[data-micro="product"]');
 if (pw_elements.length > 0) {
   let pw_should_request = true;
+  (async () => {
     if (sessionStorage.getItem('pw_data_updated_at') === null) {
       pw_should_request = true;
     } else if (sessionStorage.getItem('pw_data_updated_at') < new Date().getTime() - 1000 * 60 * 60 * 24) {
@@ -32,15 +32,17 @@ if (pw_elements.length > 0) {
       pw_should_request = false;
     }
     if (pw_should_request) {
-      clearIndexedDB();
+      clearLocalStorage();
       sessionStorage.setItem('pw_data_updated_at', new Date().getTime());
-      sendGetRequest(pw_project_id).then((response) => {
-        initPreviewImages(pw_elements, response).then(() => {checkForNewProducts();});
-      })
-    } else {
-      checkForNewProducts();
+      await sendGetRequest(pw_project_id).then(
+        (response) => {
+          initPreviewImages(pw_elements, response);
+        }
+      )
     }
-} 
+  })()
+}
+checkForNewProducts();
 
 function checkForNewProducts() {
   let pw_missing_products = [];
@@ -70,11 +72,14 @@ function getAdditionalProducts(pw_missing_products) {
   })();
 }
 
+function removeDuplicates(arr) {
+  return Array.from(new Set(arr));
+}
+
 function stopLooping(pw_element) {
   clearInterval(pw_running_interval);
-  clearInterval();
-  pw_is_running = false;
-  if (pw_r_t_d === '1' && pw_image_prefix) {
+  //pw_running_interval = null;
+  if (pw_return_to_default === '1' && pw_image_prefix) {
     const pw_product_element = pw_element.target;
     const pw_img = pw_product_element.querySelector('img[data-micro], img[data-micro-image]');
     const pw_id = pw_product_element.getAttribute('data-micro-identifier');
@@ -114,8 +119,8 @@ var pw_enter = function(pw_element) {
   const pw_product = getProduct(pw_id);
   let pw_current_img = pw_img.src;
   pw_image_prefix = pw_current_img;
-  let pw_current_img_a = pw_current_img.split('/');
-  pw_current_img = pw_current_img_a.pop();
+  let pw_current_img_array = pw_current_img.split('/');
+  pw_current_img = pw_current_img_array[pw_current_img_array.length - 1];
   pw_image_prefix = pw_image_prefix.substring(0, pw_image_prefix.length - pw_current_img.length);
   let pw_index = getIndex(pw_product, pw_current_img);
   const cycleImages = () => {
@@ -130,8 +135,8 @@ var pw_enter = function(pw_element) {
     }
     pw_img.src = pw_image_prefix + pw_image_name;
     
-    if (pw_i_r === '0' && pw_index === pw_product.images.length - 1) {
-      if (pw_r_t_d === '1') {
+    if (pw_infinite_repeat === '0' && pw_index === pw_product.images.length - 1) {
+      if (pw_return_to_default === '1') {
         const pw_image_name_default = pw_product.images[0];
         if (pw_image_name_default !== undefined) {
           pw_img.src = pw_image_prefix + pw_image_name_default;
@@ -140,10 +145,9 @@ var pw_enter = function(pw_element) {
       stopLooping(pw_element);
     }
   };
-  setTimeout(cycleImages, pw_i_l);
-  const pw_interval_id = setInterval(cycleImages, pw_s_t);
+  setTimeout(cycleImages, pw_initial_loop);
+  const pw_interval_id = setInterval(cycleImages, pw_show_time);
   pw_running_interval = pw_interval_id;
-  pw_is_running = true;
 }; 
 
 var pw_leave = function(pw_element) {
@@ -175,6 +179,7 @@ function handleTouchMove(pw_element) {
     pw_current_img = getImageName(pw_current_img);
     pw_image_prefix = pw_image_prefix.substring(0, pw_image_prefix.length - pw_current_img.length);
     let pw_index = getIndex(pw_product, pw_current_img);
+    let pw_init_index = pw_index;
     if (pw_x_diff > 0) {
       if (pw_index === 0) {
         pw_index = pw_product.images.length - 1;
@@ -198,8 +203,10 @@ function handleTouchMove(pw_element) {
     if (isCircles()) {
       const pw_svgs = pw_link.querySelectorAll('svg');
       pw_svgs.forEach((pw_svg, i) => {
-        pw_svg.classList.remove('pw-circle');
-        pw_svg.classList.add('pw-empty-circle');
+        if (pw_init_index === i) {
+          pw_svg.classList.remove('pw-circle');
+          pw_svg.classList.add('pw-empty-circle');
+        }
         if (pw_index === i) {
           pw_svg.classList.remove('pw-empty-circle');
           pw_svg.classList.add('pw-circle');
@@ -226,14 +233,26 @@ function findParentElementByClassName(pw_element, pw_class_name) {
 }
 
 async function sendGetRequest(pw_project_id) {
+  return new Promise(async (resolve, reject) => {
     try {
       let pw_url = 'https://slabihoud.cz/images/' + pw_project_id + '/' + stringToIntModulo11(pw_project_id);
-      const response = await fetch(pw_url);
-      const pw_json_data = await response.json();
-      return pw_json_data;
+      const pw_xhr = new XMLHttpRequest();
+      pw_xhr.open('GET', pw_url, true);
+      pw_xhr.onreadystatechange = async function () {
+        if (pw_xhr.readyState === 4) {
+          if (pw_xhr.status === 200) {
+            let pw_response = parseJSONToPwProductsResponse(pw_xhr.responseText);
+            resolve(pw_response);
+          } else {
+            reject(new Error('XHR request failed'));
+          }
+        }
+      };
+      pw_xhr.send();
     } catch (error) {
       reject(error);
     }
+  });
 }
 
 function stringToIntModulo11(inputString) {
@@ -261,50 +280,46 @@ function parseJSONToPwProductsResponse(jsonData) {
   }
 }
 
-async function initPreviewImages(pw_elements, response) {
-  const db = await setupIndexedDB();
-  const transaction = db.transaction(["pw_images"], "readwrite");
-  const objectStore = transaction.objectStore("pw_images");
+function initPreviewImages(pw_elements, response) {
   for (const [index, value] of Object.entries(response)) {
-    objectStore.add({ id: index, images: value });
+    localStorage.setItem('pw_' + index, value);
   }
-  for(const pw_element of pw_elements) {
+  for (let i = 0; i < pw_elements.length; i++) {
+    let pw_skip = false;
+    const pw_element = pw_elements[i];
     pw_element.setAttribute('data-pw-init', true);
     const microDataValue = pw_element.getAttribute('data-micro-identifier');
     if (microDataValue === null) {
       continue;
     }
-    const pw_idb_data = await new Promise((resolve, reject) => {
-      const request = objectStore.get(microDataValue);
-      request.onsuccess = function (event) {
-        resolve(event.target.result);
-      };
-      request.onerror = function (event) {
-        reject(event.error);
-      };
-    });
+    const pw_local_storage_data = localStorage.getItem('pw_' + microDataValue);
 
-    if (!pw_idb_data || !pw_idb_data.images) {
+    if (pw_local_storage_data === null) {
       continue;
     }
-
     const pw_image = pw_element.querySelector('img[data-micro], img[data-micro-image]');
-    let pw_images = pw_idb_data.images;
-    if (pw_image.src === null) {
+    let pw_images = pw_local_storage_data.split(',');
+    let pw_first_image = getImageName(pw_image.getAttribute('data-src'));
+    pw_images.unshift(pw_first_image);
+    pw_images = removeDuplicates(pw_images);
+    if (pw_images[0] === 'undefined' || pw_images.length === 1) {
       continue;
     }
     pw_products.push({ id: microDataValue, images: pw_images});
 
-    if (isPc() && pw_images.length > 1) {
+    if (isPc()) {
       pw_element.addEventListener('mouseenter', pw_enter, false);
       pw_element.addEventListener('mouseleave', pw_leave, false); 
     }
     pw_image.setAttribute('data-micro-identifier-parent', microDataValue);
     pw_image.addEventListener('error', function handleError() {
       removeMissingImage(pw_image.src, pw_image.getAttribute('data-micro-identifier-parent'));
+      pw_skip = true;
     });
-
-    if (isMobile() && pw_images.length > 1) {
+    if (pw_skip) {
+      continue;
+    }
+    if (isMobile(pw_images)) {
       pw_image.addEventListener('touchstart', handleTouchStart, false);
       pw_image.addEventListener('touchmove', handleTouchMove, false);
       pw_image.classList.add("overlay-on");
@@ -329,9 +344,9 @@ async function initPreviewImages(pw_elements, response) {
   pw_global_products = pw_products;
 }
 
-async function removeMissingImage(pw_missing_source, pw_product_identifier) {
-  let pw_missing_source_a = pw_missing_source.split('/');
-  pw_missing_source = pw_missing_source_a[pw_missing_source_a.length - 1];
+function removeMissingImage(pw_missing_source, pw_product_identifier) {
+  let pw_missing_source_array = pw_missing_source.split('/');
+  pw_missing_source = pw_missing_source_array[pw_missing_source_array.length - 1];
   for (let i = 0; i < pw_global_products.length; i++) {
     if (pw_global_products[i].id === pw_product_identifier) {
       for (let j = 0; j < pw_global_products[i].images.length; j++) {
@@ -343,85 +358,67 @@ async function removeMissingImage(pw_missing_source, pw_product_identifier) {
       break;
     }
   }
-  const transaction = db.transaction(["pw_images"], "readwrite");
-  const objectStore = transaction.objectStore("pw_images");
-  let pw_data = await objectStore.get(microDataValue);
-  let pw_idb_a = pw_data.split(',');
-  let pw_nidb = '';
-  for (let i = 0; i < pw_idb_a.length; i++) {
-    if (pw_idb_a[i] === pw_missing_source) {
+  let pw_local_storage_data = localStorage.getItem('pw_' + pw_product_identifier);
+  let pw_local_storage_data_array = pw_local_storage_data.split(',');
+  let pw_new_local_storage_data = '';
+  for (let i = 0; i < pw_local_storage_data_array.length; i++) {
+    if (pw_local_storage_data_array[i] === pw_missing_source) {
       continue;
     }
-    pw_nidb = pw_nidb + pw_idb_a[i];
-    if (i !== pw_idb_a.length - 1) {
-      pw_nidb = pw_nidb + ',';
+    pw_new_local_storage_data = pw_new_local_storage_data + pw_local_storage_data_array[i];
+    if (i !== pw_local_storage_data_array.length - 1) {
+      pw_new_local_storage_data = pw_new_local_storage_data + ',';
     }
   }
-  objectStore.add({ id: pw_product_identifier, images: pw_nidb });
+  localStorage.setItem('pw_' + pw_product_identifier, pw_new_local_storage_data);
 }
 
 function getImageName(pw_current_img)
 {
-  if (pw_current_img === null) {
-    return null;
-  }
-  let pw_current_img_a = pw_current_img.split('/');
-  let result = pw_current_img_a.pop();
+  let pw_current_img_array = pw_current_img.split('/');
+  let result = pw_current_img_array[pw_current_img_array.length - 1];
   return result.split('?')[0];
 }
 
-function firstImageReturn() {
-  if (pw_r_t_d === '1' && pw_is_running === false && isMobile() === false) {
-    for (let i = 0; i < pw_global_products.length; i++) {
+function firstImageReturn()
+{
+  if (pw_return_to_default === '1' && pw_running_interval === null) {
+    for(let i = 0; i < pw_global_products.length; i++) {
       const pw_product = pw_global_products[i];
       const pw_product_element = document.querySelector('[data-micro-identifier="' + pw_product.id + '"]');
-      if (pw_product_element) {
-        const pw_img = pw_product_element.querySelector('img[data-micro], img[data-micro-image]');
-        if (pw_img && pw_product.images.length > 1) {
-          let pw_initial_image = pw_img.getAttribute('data-src');
-          if (pw_initial_image != null && pw_initial_image != pw_img.src) {
-            pw_img.src = pw_initial_image;
-          }
+      const pw_img = pw_product_element.querySelector('img[data-micro], img[data-micro-image]');
+      if (pw_product.images.length > 1) {
+        let pw_initial_image = pw_img.getAttribute('data-src');
+        if (pw_initial_image != null && pw_initial_image != pw_img.src ) {
+          pw_img.src = pw_initial_image;
         }
       }
     }
   }
 }
-function isMobile()
+
+function isMobile(pw_images)
 {
-  return screen.width < 768 && (pw_a_t === 'all' || pw_a_t === 'mobile');
+  return pw_images && pw_images.length > 1 && screen.width < 768 && (pw_apply_to === 'all' || pw_apply_to === 'mobile');
 }
 
 function isPc() {
-  return pw_a_t === 'all' || pw_a_t === 'pc';
+  return pw_apply_to === 'all' || pw_apply_to === 'pc';
 }
 
 function isCircles() {
-  return pw_m_i === 'circles';
+  return pw_mobile_icons === 'circles';
 }
 
 function isNumbers() {
-  return pw_m_i === 'numbers';
+  return pw_mobile_icons === 'numbers';
 }
 
-function clearIndexedDB() {
-  indexedDB.deleteDatabase('pw_db');
-}
-
-async function setupIndexedDB() {
-  return new Promise((resolve, reject) => {
-    const dbName = "pw_db";
-    const request = indexedDB.open(dbName, 1);
-
-    request.onupgradeneeded = function(event) {
-      const db = event.target.result;
-      const objectStore = db.createObjectStore("pw_images", { keyPath: "id" });
-      objectStore.createIndex("images", "images", { unique: false });
-    };
-
-    request.onsuccess = function(event) {
-      const db = event.target.result;
-      resolve(db);
-    };
-  });
+function clearLocalStorage() {
+  for (let i = 0; i < localStorage.length; i++) {
+    const pw_key = localStorage.key(i);
+    if (pw_key.startsWith('pw_')) {
+      localStorage.removeItem(pw_key);
+    }
+  }
 }
