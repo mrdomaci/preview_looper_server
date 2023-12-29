821ac2260836a11f1a7e1fb06589d2f75aea964e
Honza_Slabihoud <jan.slabihoud@4camping.cz>
2023-12-29 18:14:39 +0100

    fix url encode time

--- app/Helpers/DateTimeHelper.php
+++ app/Helpers/DateTimeHelper.php
@@ -12,7 +12,7 @@ class DateTimeHelper
         $dateTimeString = $dateTime->format('Y-m-d');
         $dateTimeString .= 'T';
         $dateTimeString .= $dateTime->format('H:i:s');
-        $dateTimeString .= '%2B0200';
-        return $dateTimeString;
+        $dateTimeString .= '+0100';
+        return urlencode($dateTimeString);
     }
 }
